4a985f87f26512984b28dad7d931ae76840beaf9
SmallGoodSergio <sergioandressc2018@gmail.com>
2025-11-18 19:41:56 -0500

    Finalizando ajustes

--- javascript/indexjava.js
+++ javascript/indexjava.js
@@ -5,3 +5,23 @@ botonNoti.addEventListener("click", () => {
     panelNoti.classList.toggle("panel-visible");
     panelNoti.classList.toggle("panel-hidden");
 });
+
+document.addEventListener("DOMContentLoaded", () => {
+
+    document.getElementById("btnInfoComida").addEventListener("click", () => {
+        window.location.href = "pages/informacionComida/subpaginasInformacion/registrarAlimentos.html";
+    });
+
+    document.getElementById("btnPlanMeta").addEventListener("click", () => {
+        window.location.href = "../../../pages/planificarMeta/subpaginasplanificar/crearMetas.html";
+    });
+
+    document.getElementById("btnRecomendaciones").addEventListener("click", () => {
+        window.location.href = "pages/recomendar/recomendaciones.html";
+    });
+
+    document.getElementById("btnConfig").addEventListener("click", () => {
+        window.location.href = "pages/configuracion.html";
+    });
+
+});
