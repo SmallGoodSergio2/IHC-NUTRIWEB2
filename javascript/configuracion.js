27f06d5b847c38dab21a64ccc15e371942a7c387
marcoestebanvargadparedes5-sys <marcoestebanvargadparedes5@gmail.com>
2025-12-02 20:04:47 -0500

    Delete javascript directory
    eliminando para actualizar archivos

--- javascript/configuracion.js
+++
@@ -1,8 +0,0 @@
-function openSidebar() {
-    document.getElementById("sidebar").classList.add("open");
-}
-
-function closeSidebar() {
-    document.getElementById("sidebar").classList.remove("open");
-}
-
